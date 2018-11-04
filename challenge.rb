2265ae0dce677207cde6110d681c7ef537ab7778
Laura S <laurasiegel10@gmail.com>
2018-11-04 20:23:30 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -7,6 +7,8 @@ badge_maker("laura")
 
 speaker_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
+rooms = ["1", "2", "3", "4", "5", "6", "7"]
+
 speaker_names.each do |name|
   puts "Hello, my name is #{name}."
 end 
