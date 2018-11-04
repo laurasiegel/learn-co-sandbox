2ca5dab7ae0724cc5945e340534d7832a3f86b1d
Laura S <laurasiegel10@gmail.com>
2018-11-04 20:47:43 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -12,8 +12,6 @@ rooms = ["1", "2", "3", "4", "5", "6", ]
 speaker_names.each do |name|
   puts "Hello, my name is #{name}."
 end 
-
-
   
     speaker_names.map do |name| |room|
     puts "Hello #{name}. You will be assigned to #{room}." 
