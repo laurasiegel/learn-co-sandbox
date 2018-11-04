72e6b7d47f409d4e58d9e468141bd5eab172f44a
Laura S <laurasiegel10@gmail.com>
2018-11-04 20:53:43 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -10,7 +10,7 @@ speaker_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 rooms = ["1", "2", "3", "4", "5", "6", "7"]
 
 speaker_names.each do |name|
-  puts "Hello, my name is #{name}."
+  puts "Hello, my name is #{name}. you are assigned to #{room}."
 end 
 
 puts assignments = speaker_names.zip(rooms)
