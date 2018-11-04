ef519836a7562ff755235356346345fca7da0805
Laura S <laurasiegel10@gmail.com>
2018-11-04 20:41:52 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -7,13 +7,15 @@ badge_maker("laura")
 
 speaker_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
+rooms = ["1", "2", "3", "4", "5", "6", ]
 
 speaker_names.each do |name|
   puts "Hello, my name is #{name}."
 end 
 
-
+def assign_rooms(speaker_names)
   
-  speaker_names.map do |name|
-    puts "Hello #{name}. You will be assigned to "
+    speaker_names.map do |name|
+    puts "Hello #{name}. You will be assigned to" 
   
+end
